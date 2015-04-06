df44bf72e6e506f4d91806b89fd9b5b9bdd9667d
talespadua <talescpadua@gmail.com>
2015-04-06 15:23:44 -0300

    first commit

---
+++ gif4you.js
@@ -0,0 +1,135 @@
+var fs = require('fs');
+var path = require('path');
+var gui = require('nw.gui');
+var settings = require('./config.json');
+
+var win = gui.Window.get();
+
+var uploadElement = document.getElementById('getFile');
+var openFileButton = document.getElementById('openFile');
+var slideshow = document.getElementById('slideshow');
+var gifPreview = document.getElementById('gifPreview');
+var writeButton = document.getElementById('generateGif');
+
+var paths = [];
+var gifImage;
+
+var pad = function (n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+var openFileUpload = function(){
+	uploadElement.click();
+};
+
+var openSlideshow = function(){
+	var slide = gui.Window.open('slideshow.html');
+};
+
+var decodeBase64Image = function(dataString){
+	var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+	response = {};
+
+	if (matches.length !== 3) {
+		return new Error('Invalid input string');
+	}
+
+	response.type = matches[1];
+	response.data = new Buffer(matches[2], 'base64');
+
+	return response;
+}
+
+var writeSettings = function(){
+	var data = JSON.stringify(settings, null, 4);
+
+	fs.writeFile('config.json', data, function(err){
+		if (err) throw err
+		console.log('Settings File updated');
+	})
+}
+
+var writeGif = function(){
+	console.log(gifImage);	
+
+	var imageBuffer = decodeBase64Image(gifImage);
+
+	var filename = settings.gifName + pad(settings.gifNumber, 4) + ".gif";
+
+	fs.writeFile('gifs/' + filename, imageBuffer.data, function(err){
+		if (err) throw err
+		console.log('File saved as ' + filename);
+		alert("Arquivo salvo como: " + filename);
+
+		settings.gifNumber += 1;
+
+		writeSettings();
+	})
+}
+
+var selectGifs = function () {
+	var images = uploadElement.files;
+	if(images.length == 0){
+		return;
+	}
+	
+	deleteImg();
+
+	for(var i = 0; i < images.length; i++){
+		paths.push(images[i].path);
+	}
+
+	gifshot.createGIF({
+		images: paths,
+		gifWidth: 300,
+		gifHeight: 500,
+		interval:0.2
+	} ,function(obj) {
+		if(!obj.error) {
+			var image = obj.image,
+			animatedImage = document.createElement('img');
+			gifImage = image;
+			animatedImage.id = "gifID";
+			animatedImage.src = image;
+			gifPreview.appendChild(animatedImage);
+		}
+	});
+};
+
+var deleteImg = function(){
+	paths = [];
+	var imgtag = document.getElementById('gifID');
+	if(imgtag != null){
+		imgtag.parentNode.removeChild(imgtag);
+	}
+	console.log(imgtag);
+}
+
+if (window.addEventListener) {
+	openFileButton.addEventListener('click', openFileUpload);
+	uploadElement.addEventListener('change', selectGifs);
+	slideshow.addEventListener('click', openSlideshow)
+	writeButton.addEventListener('click', writeGif)
+} else {
+	openFileButton.attachEvent('onclick', openFileUpload);
+	uploadElement.attachEvent('onchange', selectGifs);
+	slideshow.attachEvent('onclick', openSlideshow);
+	writeButton.attachEvent('onclick', writeGif);
+}
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 27){
+        win.close();
+    }
+    if(e.keyCode == 122){
+    	var isfull = win.isFullscreen;
+    	if (isfull){
+    		win.leaveFullscreen();
+    	}
+    	else{
+    		win.enterFullscreen();
+    	}
+    }
+}
---
+++ slideshow.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+var path = require('path');
+var gui = require('nw.gui');
+var win = gui.Window.get();
+
+var gifs = [];
+var gifPlace = document.getElementById('gifdiv');
+var n = 0;
+
+fs.readdir('gifs', getFileNames);
+
+function getFileNames(err, files) {
+  if (err) {
+    throw err;
+  }
+  gifs = [];
+  files.forEach(forEachFile);
+}
+
+function forEachFile(file) {
+	fs.lstat('/'+file, function(err, stats){
+		if (path.extname(file) == ".gif") {
+			gifs.push(file);
+		}
+	});
+}
+
+function doTheThings(){
+	fs.readdir('gifs', getFileNames);
+	changeGif();
+	console.log("Doing the things")
+}
+
+function changeGif(){
+	// gifPlace.setAttribute("background-image", );
+	var url = "url(gifs/" + gifs[n] + ")";
+	document.body.style.backgroundImage = url;
+
+	if (n >= gifs.length -1) {
+		n = 0;
+	} else {
+		n++;
+	}
+}
+
+setInterval(doTheThings, 1000 * 4);
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 27){
+        win.close();
+    }
+    if(e.keyCode == 122){
+    	var isfull = win.isFullscreen;
+    	if (isfull){
+    		win.leaveFullscreen();
+    	}
+    	else{
+    		win.enterFullscreen();
+    	}
+    }
+}
